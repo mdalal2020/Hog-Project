956eeb08a038352824fc85651fce3cd93a42ec4e
mdalal2020 <mdalal@berkeley.edu>
2016-09-02 18:36:02 -0700

    Update Masterhog.py

--- Masterhog.py
+++ Masterhog.py
@@ -1,1 +1,43 @@
-cool
+"""CS 61A Presents The Game of Hog."""
+
+from dice import four_sided, six_sided, make_test_dice
+from ucb import main, trace, log_current_line, interact
+
+GOAL_SCORE = 100  # The goal of Hog is to score 100 points.
+
+
+######################
+# Phase 1: Simulator #
+######################
+
+def roll_dice(num_rolls, dice=six_sided):
+    """Simulate rolling the DICE exactly NUM_ROLLS>0 times. Return the sum of
+    the outcomes unless any of the outcomes is 1. In that case, return the
+    number of 1's rolled.
+    """
+    # These assert statements ensure that num_rolls is a positive integer.
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls > 0, 'Must roll at least once.'
+    # BEGIN PROBLEM 1
+    sum = 0
+    num1s = 0
+    current_roll = 0
+    while(num_rolls>0):
+        current_roll = dice()
+        if(current_roll == 1):
+            num1s+=1
+        sum+=current_roll
+        num_rolls-=1
+    if(num1s>0):
+        return num1s
+    return sum
+
+    # END PROBLEM 1
+
+
+def free_bacon(opponent_score):
+    """Return the points scored from rolling 0 dice (Free Bacon)."""
+    # BEGIN PROBLEM 2
+    #returnns 1 plus the larger digit of the two numbers
+    return 1 + max(opponent_score % 10, opponent_score // 10)
+    # END PROBLEM 2
